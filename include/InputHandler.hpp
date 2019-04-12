9166d7570df97e530a754d41e875fcae542cfdc9
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2019-04-12 20:51:30 +0300

    Add InputHandler

---
+++ include/InputHandler.hpp
@@ -0,0 +1,38 @@
+#ifndef PROSPER_INPUTHANDLER_HPP
+#define PROSPER_INPUTHANDLER_HPP
+
+#include <glm/glm.hpp>
+
+class InputHandler {
+public:
+    struct Mouse {
+        glm::vec2 currentPos;
+        glm::vec2 lastPos;
+        bool leftDown;
+        bool rightDown;
+
+        Mouse() :
+            currentPos(0.f),
+            lastPos(0.f),
+            leftDown(false),
+            rightDown(false)
+        { }
+    };
+
+    InputHandler(const InputHandler& other) = delete;
+    InputHandler& operator= (const InputHandler& other) = delete;
+
+    static InputHandler& instance();
+    const Mouse& mouse() const;
+
+private:
+    InputHandler() = default;
+
+    Mouse _mouse;
+
+    // Window updates input state directly
+    friend class Window;
+
+};
+
+#endif // PROSPER_INPUTHANDLER_HPP
---
+++ src/InputHandler.cpp
@@ -0,0 +1,12 @@
+#include "InputHandler.hpp"
+
+InputHandler& InputHandler::instance()
+{
+    static InputHandler ih;
+    return ih;
+}
+
+const InputHandler::Mouse& InputHandler::mouse() const
+{
+    return _mouse;
+}
