67c86de1b6c00f915f5665117722584e7ad2fdb6
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2019-08-17 12:23:37 +0300

    main: Remove catchall exception handling
    All exceptions currently lead to failures and obfuscating errors makes
    little sense here.

--- src/main.cpp
+++ src/main.cpp
@@ -6,19 +6,8 @@
 
 int main() {
     App app;
-
-#ifdef NDEBUG
-    try {
-        app.init();
-        app.run();
-    } catch (const std::exception& e) {
-        std::cerr << e.what() << std::endl;
-        return EXIT_FAILURE;
-    }
-#else
     app.init();
     app.run();
-#endif // DEBUG
 
     return EXIT_SUCCESS;
 }
