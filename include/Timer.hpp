2c93426edb40fcf283f023f7d8379c3ac93587c3
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2021-11-14 09:41:06 +0200

    Add Timer

---
+++ include/Timer.hpp
@@ -0,0 +1,18 @@
+#ifndef TIMER_HPP
+#define TIMER_HPP
+
+#include <chrono>
+
+class Timer {
+  public:
+    Timer();
+    ~Timer() {}
+
+    void reset();
+    float getSeconds() const;
+
+  private:
+    std::chrono::time_point<std::chrono::system_clock> _start;
+};
+
+#endif // TIMER_HPP
