3b43a1577f0668c1be6276cca525760d64d8b75a
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2021-12-02 19:59:54 +0200

    VkUtils: Add flag checks

--- include/VkUtils.hpp
+++ include/VkUtils.hpp
@@ -5,4 +5,16 @@
 
 void checkSuccess(vk::Result result, const char *source);
 
+template <typename T, typename V> bool containsFlag(T mask, V flag)
+{
+    return (mask & flag) == flag;
+}
+
+template <typename T, typename V>
+void assertContainsFlag(T mask, V flag, const char *errMsg)
+{
+    if (!containsFlag(mask, flag))
+        throw std::runtime_error(errMsg);
+}
+
 #endif // PROSPER_VKUTILS_HPP
