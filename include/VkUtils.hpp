4c499fc085d04ea390aceaa773f5bf7063caca3a
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2022-06-14 19:07:04 +0300

    VkUtils: Add aspectMask()
    This can come from the format so let's do that

--- include/VkUtils.hpp
+++ include/VkUtils.hpp
@@ -17,4 +17,24 @@ void assertContainsFlag(T mask, V flag, const char *errMsg)
         throw std::runtime_error(errMsg);
 }
 
+constexpr vk::ImageAspectFlags aspectMask(vk::Format format)
+{
+    switch (format)
+    {
+    case vk::Format::eD16Unorm:
+    case vk::Format::eX8D24UnormPack32:
+    case vk::Format::eD32Sfloat:
+        return vk::ImageAspectFlagBits::eDepth;
+    case vk::Format::eS8Uint:
+        return vk::ImageAspectFlagBits::eStencil;
+    case vk::Format::eD16UnormS8Uint:
+    case vk::Format::eD24UnormS8Uint:
+    case vk::Format::eD32SfloatS8Uint:
+        return vk::ImageAspectFlagBits::eDepth |
+               vk::ImageAspectFlagBits::eStencil;
+    default:
+        return vk::ImageAspectFlagBits::eColor;
+    }
+}
+
 #endif // PROSPER_VKUTILS_HPP
