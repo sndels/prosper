fdc57a9b986a1b2b1eb0402ac5982c1fb0692217
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2022-10-22 11:43:44 +0300

    shaders: Use common debug draw in RT

--- res/shader/rt/rtpc.glsl
+++ res/shader/rt/rtpc.glsl
@@ -4,8 +4,6 @@
 layout(push_constant) uniform RtPC { uint drawType; }
 rtPC;
 
-const uint DrawType_PrimitiveID = 0;
-const uint DrawType_MeshID = 1;
-const uint DrawType_MaterialID = 2;
+#define CommonDebugDrawTypeOffset 0
 
 #endif // RTPC_GLSL
