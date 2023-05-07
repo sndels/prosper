d59af827f2635743ff5db5197f343704868e220b
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2023-05-07 14:25:45 +0300

    camera: Add depth linearization, world pos reconstruction

--- res/shader/camera.glsl
+++ res/shader/camera.glsl
@@ -13,4 +13,25 @@ layout(set = CAMERA_SET, binding = 0) uniform Camera
 }
 camera;
 
+float linearizeDepth(float nonLinearDepth)
+{
+    // vec4 tmp = inverse(camera.cameraToClip) * vec4(0, 0, depth, 1);
+    // return tmp.z / tmp.w;
+    // TODO:
+    // This is "optimized" version is different for orthographic for e.g. sun
+    // shadows. Ortho should be detectable by proj[3][3] or the linearization
+    // could be parametrized differently and stored in the same constants for
+    // both projection types
+    return -camera.cameraToClip[3][2] /
+           (nonLinearDepth + camera.cameraToClip[2][2]);
+}
+
+vec3 worldPos(vec2 uv, float nonLinearDepth)
+{
+    // TODO: This is broken
+    // TODO: Cheaper view ray
+    vec4 surfaceVS = camera.clipToWorld * vec4(uv * 2 - 1, nonLinearDepth, 1);
+    return surfaceVS.xyz / surfaceVS.w;
+}
+
 #endif // CAMERA_GLSL
