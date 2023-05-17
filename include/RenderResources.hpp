dc0a778cb64c79ca86865f0df54583135d9e51b8
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2023-05-17 19:27:16 +0300

    RenderResources: Add resource collections

--- include/RenderResources.hpp
+++ include/RenderResources.hpp
@@ -4,15 +4,29 @@
 #include "DebugGeometry.hpp"
 #include "DescriptorAllocator.hpp"
 #include "Device.hpp"
+#include "RenderResourceCollection.hpp"
 #include "Utils.hpp"
 
 #include <wheels/allocators/allocator.hpp>
+#include <wheels/containers/array.hpp>
 #include <wheels/containers/static_array.hpp>
+
+using TexelBufferHandle = RenderResourceHandle<TexelBuffer>;
+using ImageHandle = RenderResourceHandle<Image>;
 
 // Renderpasses that create the resources are responsible for their recreation,
 // lifetime
-struct RenderResources
+class RenderResources
 {
+  public:
+    using RenderTexelBufferCollection = RenderResourceCollection<
+        TexelBufferHandle, TexelBuffer, TexelBufferDescription,
+        TexelBufferCreateInfo, BufferState, vk::BufferMemoryBarrier2,
+        vk::Buffer, VkBuffer, vk::ObjectType::eBuffer>;
+    using RenderImageCollection = RenderResourceCollection<
+        ImageHandle, Image, ImageDescription, ImageCreateInfo, ImageState,
+        vk::ImageMemoryBarrier2, vk::Image, VkImage, vk::ObjectType::eImage>;
+
     struct Images
     {
         Image sceneColor;
@@ -41,15 +55,44 @@ struct RenderResources
     // Both alloc and device need to live as long as this
     RenderResources(wheels::Allocator &alloc, Device *device)
     : staticDescriptorsAlloc{alloc, device}
+    , images{alloc, device}
+    , texelBuffers{alloc, device}
     {
+    }
+    ~RenderResources() = default;
+
+    RenderResources(RenderResources &) = delete;
+    RenderResources(RenderResources &&) = delete;
+    RenderResources &operator=(RenderResources &) = delete;
+    RenderResources &operator=(RenderResources &&) = delete;
+
+    // Should be called at the start of the frame so resources will get the
+    // correct names set
+    void clearDebugNames()
+    {
+        images.clearDebugNames();
+        texelBuffers.clearDebugNames();
+    }
+
+    // Should be called e.g. when viewport is resized since the render resources
+    // will be created with different sizes on the next frame
+    void destroyResources()
+    {
+        images.destroyResources();
+        texelBuffers.destroyResources();
     }
 
     // This allocator should only be used for the descriptors that can live
     // until the end of the program. As such, reset() shouldn't be called so
     // that users can rely on the descriptors being there once allocated.
+    // TODO: Does this have to be here? Shouldn't this only be used to construct
+    // things up front?
     DescriptorAllocator staticDescriptorsAlloc;
     Images staticImages;
     Buffers staticBuffers;
+
+    RenderImageCollection images;
+    RenderTexelBufferCollection texelBuffers;
 };
 
 #endif // PROSPER_RENDER_RESOURCES_HPP
