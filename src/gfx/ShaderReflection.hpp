b61bc17333c49b4e0d8844f2a06d19db34339cf1
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2023-09-26 18:06:26 +0300

    ShaderReflection: Add dynamicBuffers
    Required to generate correct descriptor types. Matching on name should be
    good enough.

--- src/gfx/ShaderReflection.hpp
+++ src/gfx/ShaderReflection.hpp
@@ -33,7 +33,8 @@ class ShaderReflection
     ShaderReflection(wheels::Allocator &alloc);
     ShaderReflection(
         wheels::ScopedScratch scopeAlloc, wheels::Allocator &alloc,
-        wheels::Span<const uint32_t> spvWords);
+        wheels::Span<const uint32_t> spvWords,
+        wheels::Span<const wheels::String> dynamicBuffers = {});
     ~ShaderReflection() = default;
 
     ShaderReflection(const ShaderReflection &) = delete;
