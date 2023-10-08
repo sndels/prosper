1b818a8c606fcab8253eb86ed7ed95991824c3f7
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2023-10-08 14:25:07 +0300

    Add accessors

---
+++ src/scene/Accessors.cpp
@@ -0,0 +1,102 @@
+#include "Accessors.hpp"
+
+#include "../utils/Utils.hpp"
+#include <glm/gtc/quaternion.hpp>
+
+using namespace glm;
+
+TimeAccessor::TimeAccessor(
+    const float *data, uint32_t count, const Interval &interval)
+: _data{data}
+, _count{count}
+, _interval{interval}
+{
+    assert(_data != nullptr);
+    assert(_count > 0);
+}
+
+float TimeAccessor::endTimeS() const { return _interval.endTimeS; }
+
+KeyFrameInterpolation TimeAccessor::interpolation(float timeS) const
+{
+    if (timeS <= _interval.startTimeS || timeS < _data[0])
+        return KeyFrameInterpolation{
+            .t = 0.f,
+            .firstFrame = 0,
+        };
+
+    if (timeS >= _interval.endTimeS)
+        return KeyFrameInterpolation{
+            .t = 0.f,
+            .firstFrame = _count - 1,
+        };
+
+    // TODO:
+    // Cache previous first frame to speed up consequtive queries. Average case
+    // is within the same interval, next one if not. Random access still
+    // requires search. Need to be careful that the cache check works with
+    // random access. Profile well.
+
+    KeyFrameInterpolation ret;
+    const uint32_t lastFrame = _count - 1;
+    while (ret.firstFrame < lastFrame)
+    {
+        const float frameTimeS = _data[ret.firstFrame];
+        // Time == first frame has an early out
+        if (frameTimeS > timeS)
+            break;
+
+        ret.firstFrame++;
+    }
+    // We went one over in the loop
+    ret.firstFrame--;
+
+    if (ret.firstFrame < lastFrame)
+    {
+        const float firstTime = _data[ret.firstFrame];
+        const float secondTime = _data[ret.firstFrame + 1];
+        assert(firstTime <= timeS);
+        assert(timeS <= secondTime);
+
+        const float duration = secondTime - firstTime;
+        ret.stepDuration = timeS - firstTime;
+        ret.t = ret.stepDuration / duration;
+    }
+    // else, firstFrame == lastFrame and t == 0.f signals we should clamp
+    assert(ret.t >= 0.f);
+    assert(ret.t <= 1.f);
+
+    return ret;
+}
+
+template <>
+ValueAccessor<vec3>::ValueAccessor(const uint8_t *data, uint32_t count)
+: _data{data}
+, _count{count}
+{
+    assert(_data != nullptr);
+    assert(_count > 0);
+}
+
+template <> vec3 ValueAccessor<vec3>::read(uint32_t index) const
+{
+    assert(index < _count);
+    return *reinterpret_cast<const vec3 *>(
+        _data + asserted_cast<size_t>(index) * 3 * sizeof(float));
+}
+
+template <>
+ValueAccessor<quat>::ValueAccessor(const uint8_t *data, uint32_t count)
+: _data{data}
+, _count{count}
+{
+    assert(_data != nullptr);
+    assert(_count > 0);
+}
+
+template <> quat ValueAccessor<quat>::read(uint32_t index) const
+{
+    assert(index < _count);
+    return *reinterpret_cast<const quat *>(
+        _data + asserted_cast<size_t>(index) * 4 * sizeof(float));
+}
---
+++ src/scene/Accessors.hpp
@@ -0,0 +1,53 @@
+#ifndef PROSPER_SCENE_ACCESSORS_HPP
+#define PROSPER_SCENE_ACCESSORS_HPP
+
+#include <cstdint>
+#include <glm/glm.hpp>
+
+struct KeyFrameInterpolation
+{
+    // If t == 0.f, use start frame directly
+    // float comparison is sus but it's guaranteed to work when we store 0.f
+    // explicitly
+    float t{0.f};
+    float stepDuration{0.f};
+    uint32_t firstFrame{0};
+};
+
+class TimeAccessor
+{
+  public:
+    struct Interval
+    {
+        float startTimeS{0.f};
+        float endTimeS{0.f};
+    };
+    TimeAccessor(const float *data, uint32_t count, const Interval &interval);
+
+    float endTimeS() const;
+    KeyFrameInterpolation interpolation(float timeS) const;
+
+  private:
+    const float *_data{nullptr};
+    uint32_t _count{0};
+    Interval _interval;
+};
+
+// Templated so that Sampler can be templated on the read value type.
+// Concrete implementations for supported types
+template <typename T> class ValueAccessor
+{
+  public:
+    // Count is for vector elements, not individual float
+    ValueAccessor(const uint8_t *data, uint32_t count);
+
+    T read(uint32_t index) const;
+
+  private:
+    const uint8_t *_data{nullptr};
+    uint32_t _count{0};
+};
+// TODO:
+// Requires or something to give an error for unimplemented value types
+
+#endif // PROSPER_SCENE_ACCESSORS_HPP
