6ac325143b631d9fe33baefc4777928231f0d707
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2023-10-22 13:47:19 +0300

    skybox: Evaluate specular IBL

--- res/shader/scene/skybox.glsl
+++ res/shader/scene/skybox.glsl
@@ -8,24 +8,39 @@ layout(set = SKYBOX_SET, binding = 3) uniform samplerCube skyboxRadiance;
 
 #include "../brdf.glsl"
 
-// Adapted from https://learnopengl.com/PBR/IBL/Diffuse-irradiance
+// Adapted from
+// https://learnopengl.com/PBR/IBL/Diffuse-irradiance
+// https://learnopengl.com/PBR/IBL/Specular-IBL
 
 vec3 evalIBL(VisibleSurface surface)
 {
     vec3 f0 = fresnelZero(surface);
 
-    vec3 F = schlickFresnelWithRoughness(
-        max(dot(surface.normalWS, surface.invViewRayWS), 0.0), f0,
-        surface.material.roughness);
+    float NoV = saturate(dot(surface.normalWS, surface.invViewRayWS));
+
+    vec3 F = schlickFresnelWithRoughness(NoV, f0, surface.material.roughness);
 
     vec3 kD = 1.0 - F;
     kD *= 1 - surface.material.metallic;
 
+    // Diffuse
     vec3 irradiance = texture(skyboxIrradiance, surface.normalWS).rgb;
     vec3 diffuse = irradiance * surface.material.albedo;
 
+    // Specular
+    vec3 R = reflect(-surface.invViewRayWS, surface.normalWS);
+    // TODO: This should be a define
+    const float MAX_REFLECTION_LOD = 10.0;
+    vec3 prefilteredRadiance =
+        textureLod(
+            skyboxRadiance, R, surface.material.roughness * MAX_REFLECTION_LOD)
+            .rgb;
+    vec2 envBrdf =
+        texture(specularBrdfLut, vec2(NoV, 1 - surface.material.roughness)).rg;
+    vec3 specular = prefilteredRadiance * (F * envBrdf.x + envBrdf.y);
+
     // TODO: Apply AO to this
-    vec3 ibl = kD * diffuse;
+    vec3 ibl = kD * diffuse + specular;
 
     return ibl;
 }
