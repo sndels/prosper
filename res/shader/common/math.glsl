cfd0b814b39c7531373a73280002eb36f1c3b4c6
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2023-11-11 14:08:32 +0200

    math: Add componentwise min/max

--- res/shader/common/math.glsl
+++ res/shader/common/math.glsl
@@ -4,4 +4,12 @@
 #define PI 3.14159265
 #define saturate(x) clamp(x, 0.0, 1.0)
 
+#define max2(v) max(v.x, v.y)
+#define max3(v) max(max(v.x, v.y), v.z)
+#define max4(v) max(max(v.x, v.y), max(v.z, v.w))
+
+#define min2(v) min(v.x, v.y)
+#define min3(v) min(min(v.x, v.y), v.z)
+#define min4(v) min(min(v.x, v.y), min(v.z, v.w))
+
 #endif // COMMON_MATH_GLSL
