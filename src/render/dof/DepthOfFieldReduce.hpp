1d0323e45acffc157a8831b1f563d641a0cc7b22
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2024-03-09 15:43:34 +0200

    Add DepthOfFieldReduce

---
+++ src/render/dof/DepthOfFieldReduce.hpp
@@ -0,0 +1,52 @@
+#ifndef PROSPER_RENDER_DEPTH_OF_FIELD_REDUCE_HPP
+#define PROSPER_RENDER_DEPTH_OF_FIELD_REDUCE_HPP
+
+#include <wheels/allocators/scoped_scratch.hpp>
+#include <wheels/containers/optional.hpp>
+#include <wheels/containers/static_array.hpp>
+
+#include "../../gfx/Fwd.hpp"
+#include "../../gfx/Resources.hpp"
+#include "../../utils/Fwd.hpp"
+#include "../../utils/Utils.hpp"
+#include "../ComputePass.hpp"
+#include "../Fwd.hpp"
+#include "../RenderResourceHandle.hpp"
+
+// Based on A Life of a Bokeh by Guillaume Abadie
+// https://advances.realtimerendering.com/s2018/index.htm
+
+class DepthOfFieldReduce
+{
+  public:
+    DepthOfFieldReduce() noexcept = default;
+    ~DepthOfFieldReduce();
+
+    DepthOfFieldReduce(const DepthOfFieldReduce &other) = delete;
+    DepthOfFieldReduce(DepthOfFieldReduce &&other) = delete;
+    DepthOfFieldReduce &operator=(const DepthOfFieldReduce &other) = delete;
+    DepthOfFieldReduce &operator=(DepthOfFieldReduce &&other) = delete;
+
+    void init(
+        wheels::ScopedScratch scopeAlloc, Device *device,
+        RenderResources *resources,
+        DescriptorAllocator *staticDescriptorsAlloc);
+
+    void recompileShaders(
+        wheels::ScopedScratch scopeAlloc,
+        const wheels::HashSet<std::filesystem::path> &changedFiles);
+
+    void record(
+        wheels::ScopedScratch scopeAlloc, vk::CommandBuffer cb,
+        const ImageHandle &inOutIlluminationMips, uint32_t nextFrame,
+        Profiler *profiler);
+
+    bool _initialized{false};
+    Device*_device{nullptr};
+    RenderResources *_resources{nullptr};
+    ComputePass _computePass;
+    Buffer _atomicCounter;
+    bool _counterNotCleared{true};
+};
+
+#endif // PROSPER_RENDER_DEPTH_OF_FIELD_REDUCE_HPP
