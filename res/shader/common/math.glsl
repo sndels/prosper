414ddbc23fd66671eeea74ca6e04923fb3d584cd
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2024-05-26 12:12:58 +0300

    math: Add hsv-rgb conversions

--- res/shader/common/math.glsl
+++ res/shader/common/math.glsl
@@ -14,4 +14,66 @@
 
 float luminance(vec3 c) { return dot(vec3(0.299, 0.587, 0.114), c); }
 
+// Returns HSV with hue unscaled, multiply it by 60 to get degrees
+vec3 rgbToHsv(vec3 rgb)
+{
+    // https://en.wikipedia.org/wiki/HSL_and_HSV
+
+    float value = max(max(rgb.r, rgb.g), rgb.b);
+    float valueMinusChroma = min(min(rgb.r, rgb.g), rgb.b);
+    float chroma = value - valueMinusChroma;
+
+    // TODO:
+    // Feels like these branches and the value/valueMinusChroma could be folded
+    // together
+    float hue;
+    if (chroma == 0.)
+        hue = 0.;
+    else if (value == rgb.r)
+        hue = mod((rgb.g - rgb.b) / chroma, 6.);
+    else if (value == rgb.g)
+        hue = (rgb.b - rgb.r) / chroma + 2.;
+    else
+        hue = (rgb.r - rgb.g) / chroma + 4.;
+
+    float saturation = value == 0. ? 0. : chroma / value;
+
+    return vec3(hue, saturation, value);
+}
+
+// Expects HSV with hue not scaled to degrees
+vec3 hsvToRgb(vec3 hsv)
+{
+    // https://en.wikipedia.org/wiki/HSL_and_HSV
+
+    float hue = hsv.r;
+    float saturation = hsv.g;
+    float value = hsv.b;
+
+    float chroma = value * saturation;
+
+    float x = chroma * (1. - abs(mod(hue, 2.) - 1.));
+
+    // TODO:
+    // That's a lot of branching. Is there a clever branchless algo here that's
+    // nicer for GPUs?
+    vec3 rgb;
+    if (hue < 1.)
+        rgb = vec3(chroma, x, 0.);
+    else if (hue < 2.)
+        rgb = vec3(x, chroma, 0.);
+    else if (hue < 3.)
+        rgb = vec3(0., chroma, x);
+    else if (hue < 4.)
+        rgb = vec3(0., x, chroma);
+    else if (hue < 5.)
+        rgb = vec3(x, 0., chroma);
+    else
+        rgb = vec3(chroma, 0., x);
+
+    float m = value - chroma;
+
+    return rgb + m;
+}
+
 #endif // COMMON_MATH_GLSL
