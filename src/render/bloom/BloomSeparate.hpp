c9ef84482130f6be12f6fec8f90862cfdb25e75c
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2024-09-22 13:26:17 +0300

    Add BloomSeparate

---
+++ src/render/bloom/BloomSeparate.hpp
@@ -0,0 +1,48 @@
+#ifndef PROSPER_RENDER_BLOOM_SEPARATE_HPP
+#define PROSPER_RENDER_BLOOM_SEPARATE_HPP
+
+#include "render/ComputePass.hpp"
+#include "render/Fwd.hpp"
+#include "render/RenderResourceHandle.hpp"
+
+#include <wheels/allocators/scoped_scratch.hpp>
+#include <wheels/containers/optional.hpp>
+
+class BloomSeparate
+{
+  public:
+    BloomSeparate() noexcept = default;
+    ~BloomSeparate() = default;
+
+    BloomSeparate(const BloomSeparate &other) = delete;
+    BloomSeparate(BloomSeparate &&other) = delete;
+    BloomSeparate &operator=(const BloomSeparate &other) = delete;
+    BloomSeparate &operator=(BloomSeparate &&other) = delete;
+
+    void init(wheels::ScopedScratch scopeAlloc);
+
+    void recompileShaders(
+        wheels::ScopedScratch scopeAlloc,
+        const wheels::HashSet<std::filesystem::path> &changedFiles);
+
+    void drawUi();
+
+    struct Input
+    {
+        ImageHandle illumination;
+    };
+    struct Output
+    {
+        ImageHandle highlights;
+    };
+    [[nodiscard]] Output record(
+        wheels::ScopedScratch scopeAlloc, vk::CommandBuffer cb,
+        const Input &input, uint32_t nextFrame);
+
+  private:
+    bool m_initialized{false};
+    float m_threshold{1.f};
+    ComputePass m_computePass;
+};
+
+#endif // PROSPER_RENDER_BLOOM_SEPARATE_HPP
---
+++ src/render/bloom/Fwd.hpp
@@ -0,0 +1,8 @@
+
+#ifndef PROSPER_RENDER_BLOOM_FWD_HPP
+#define PROSPER_RENDER_BLOOM_FWD_HPP
+
+// BloomSeparate.hpp
+class BloomSeparate;
+
+#endif // PROSPER_RENDER_BLOOM_FWD_HPP
