be927ff5cceaf665344656c48e98a6c33c5f758c
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2024-11-30 11:17:06 +0200

    Add BloomConvolution

---
+++ src/render/bloom/BloomConvolution.cpp
@@ -0,0 +1,97 @@
+#include "BloomConvolution.hpp"
+
+#include "render/RenderResources.hpp"
+#include "render/Utils.hpp"
+#include "utils/Profiler.hpp"
+
+#include <imgui.h>
+
+using namespace glm;
+using namespace wheels;
+
+namespace
+{
+constexpr uvec3 sGroupSize{16, 16, 1};
+
+ComputePass::Shader shaderDefinitionCallback(Allocator &alloc)
+{
+    return ComputePass::Shader{
+        .relPath = "shader/bloom/convolution.comp",
+        .debugName = String{alloc, "BloomConvolutionCS"},
+        .groupSize = sGroupSize,
+    };
+}
+
+} // namespace
+
+void BloomConvolution::init(ScopedScratch scopeAlloc)
+{
+    WHEELS_ASSERT(!m_initialized);
+
+    m_computePass.init(WHEELS_MOV(scopeAlloc), shaderDefinitionCallback);
+
+    m_initialized = true;
+}
+
+void BloomConvolution::recompileShaders(
+    wheels::ScopedScratch scopeAlloc,
+    const HashSet<std::filesystem::path> &changedFiles)
+{
+    WHEELS_ASSERT(m_initialized);
+
+    m_computePass.recompileShader(
+        WHEELS_MOV(scopeAlloc), changedFiles, shaderDefinitionCallback);
+}
+
+void BloomConvolution::record(
+    ScopedScratch scopeAlloc, vk::CommandBuffer cb,
+    const InputOutput &inputOutput, const uint32_t nextFrame)
+{
+    WHEELS_ASSERT(m_initialized);
+
+    PROFILER_CPU_SCOPE("  Convolution");
+
+    const vk::Extent2D highlightsExtent =
+        getExtent2D(inputOutput.inOutHighlightsDft);
+    const vk::Extent2D kernelExtent = getExtent2D(inputOutput.inKernelDft);
+    WHEELS_ASSERT(highlightsExtent.width == highlightsExtent.height);
+    WHEELS_ASSERT(kernelExtent.width == kernelExtent.height);
+    WHEELS_ASSERT(highlightsExtent.width == kernelExtent.width);
+    WHEELS_ASSERT(highlightsExtent.width % sGroupSize.x == 0);
+    WHEELS_ASSERT(highlightsExtent.height % sGroupSize.y == 0);
+
+    m_computePass.updateDescriptorSet(
+        scopeAlloc.child_scope(), nextFrame,
+        StaticArray{{
+            DescriptorInfo{vk::DescriptorImageInfo{
+                .imageView = gRenderResources.images
+                                 ->resource(inputOutput.inOutHighlightsDft)
+                                 .view,
+                .imageLayout = vk::ImageLayout::eGeneral,
+            }},
+            DescriptorInfo{vk::DescriptorImageInfo{
+                .imageView =
+                    gRenderResources.images->resource(inputOutput.inKernelDft)
+                        .view,
+                .imageLayout = vk::ImageLayout::eGeneral,
+            }},
+        }});
+
+    transition(
+        WHEELS_MOV(scopeAlloc), cb,
+        Transitions{
+            .images = StaticArray<ImageTransition, 2>{{
+                {inputOutput.inOutHighlightsDft,
+                 ImageState::ComputeShaderReadWrite},
+                {inputOutput.inKernelDft, ImageState::ComputeShaderRead},
+            }},
+        });
+
+    PROFILER_GPU_SCOPE(cb, "  GenerateKernel");
+
+    const vk::DescriptorSet descriptorSet = m_computePass.storageSet(nextFrame);
+
+    const uvec3 groupCount = m_computePass.groupCount(
+        uvec3{highlightsExtent.width, highlightsExtent.height, 1u});
+    m_computePass.record(cb, groupCount, Span{&descriptorSet, 1});
+}
---
+++ src/render/bloom/BloomConvolution.hpp
@@ -0,0 +1,41 @@
+#ifndef PROSPER_RENDER_BLOOM_CONVOLUTION_HPP
+#define PROSPER_RENDER_BLOOM_CONVOLUTION_HPP
+
+#include "render/ComputePass.hpp"
+#include "render/Fwd.hpp"
+#include "render/RenderResourceHandle.hpp"
+
+#include <wheels/allocators/scoped_scratch.hpp>
+
+class BloomConvolution
+{
+  public:
+    BloomConvolution() noexcept = default;
+    ~BloomConvolution() = default;
+
+    BloomConvolution(const BloomConvolution &other) = delete;
+    BloomConvolution(BloomConvolution &&other) = delete;
+    BloomConvolution &operator=(const BloomConvolution &other) = delete;
+    BloomConvolution &operator=(BloomConvolution &&other) = delete;
+
+    void init(wheels::ScopedScratch scopeAlloc);
+
+    void recompileShaders(
+        wheels::ScopedScratch scopeAlloc,
+        const wheels::HashSet<std::filesystem::path> &changedFiles);
+
+    struct InputOutput
+    {
+        ImageHandle inOutHighlightsDft;
+        ImageHandle inKernelDft;
+    };
+    void record(
+        wheels::ScopedScratch scopeAlloc, vk::CommandBuffer cb,
+        const InputOutput &inputOutput, uint32_t nextFrame);
+
+  private:
+    bool m_initialized{false};
+    ComputePass m_computePass;
+};
+
+#endif // PROSPER_RENDER_BLOOM_CONVOLUTION_HPP
